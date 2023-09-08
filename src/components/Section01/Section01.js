2a4079669a46dad6a5682bcc6f8453e8d45a0448
Matheus Rafael <matheusrafael77@hotmail.com>
2023-09-08 16:12:28 -0300

    comit

--- src/components/Section01/Section01.js
+++ src/components/Section01/Section01.js
@@ -3,7 +3,7 @@ import './Section01.css'
 const Section01 = () => {
   return (
     <section id="section1">
-    <div><h1>Oi, eu sou o <br /> <span>Matheus Rafael.</span> <br />
+    <div className='section1div'><h1>Oi, eu sou o <br /> <span>Matheus Rafael.</span> <br />
     Desenvolvedor front-end.</h1></div>
    </section>
   )
