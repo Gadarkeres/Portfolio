c33b8dff6b113a98f9758490177cea444fd175d4
Matheus Rafael <matheusrafael77@hotmail.com>
2023-09-08 16:26:26 -0300

    final styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
         </div>
         <div className="icones">
         <p>
-          <a href="https://www.linkedin.com/in/matheus-rafael-b0aa18259/" target='_blank' rel="noopener noreferrer"><FaLinkedin/></a>
+          <a href="https://www.linkedin.com/in/matheus-rafael-b0aa18259/" target='_blank' ><FaLinkedin/></a>
           <a href="https://github.com/Gadarkeres" target='_blank'  rel="noopener noreferrer"><FaGithub/></a>
       
 
