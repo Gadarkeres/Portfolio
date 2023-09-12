030e6591fa0fd2941bec52e037ddc6e5e2b90735
Matheus Rafael <matheusrafael77@hotmail.com>
2023-09-11 23:37:49 -0300

    update project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,7 @@ import './Projects.css'
 // componentes
 import Card from './Card'
 // imagens
-import img1 from './img/hd-filmes2.png'
+import img1 from './img/timer.png'
 import img2 from './img/tempo.png'
 import img3 from './img/email.png'
 import embreve from './img/em-breve.jpg'
@@ -17,9 +17,9 @@ const Projects = () => {
     <section id="section4">
       <h2 className="Tittle">PROJETOS</h2>
       <div className="container_card">
+        <Card img={img1} title={"Projeto pessoal timer de estudos"} link={"https://gadarkeres.github.io/Projeto-timer/"}/>
         <Card img={img2} title={'Projeto pessoal previsão do tempo'} link={"https://gadarkeres.github.io/Projeto-Tempo/"}/>
         <Card img={img3} title={'Projeto desafio validação de email'} link={"https://gadarkeres.github.io/email-validation-project/"}/>
-        <Card img={embreve} title={"EM BREVE"}/>
         <Card img={embreve}title={"EM BREVE"}/>
         <Card img={embreve}title={"EM BREVE"}/>
         <Card img={embreve}title={"EM BREVE"}/>
