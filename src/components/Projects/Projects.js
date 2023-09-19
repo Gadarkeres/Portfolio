ed1ce5f9d20785ee6c4937bad02c188b38656d04
Matheus Rafael <matheusrafael77@hotmail.com>
2023-09-19 15:12:38 -0300

    projects update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,6 +6,7 @@ import img1 from './img/timer.png'
 import img2 from './img/tempo.png'
 import img3 from './img/email.png'
 import img4 from './img/gerador.png'
+import img5 from './img/todo-list.png'
 import embreve from './img/em-breve.jpg'
 // pdf
 import pdf from './file/Curriculo-Matheus-Att.pdf'
@@ -18,11 +19,12 @@ const Projects = () => {
     <section id="section4">
       <h2 className="Tittle">PROJETOS</h2>
       <div className="container_card">
+      <Card img={img5}title={"Lista de tarefas automática"} link="https://gadarkeres.github.io/todo-list/"/>
         <Card img={img1} title={"Projeto: Timer de estudos"} link={"https://gadarkeres.github.io/Projeto-timer/"}/>
         <Card img={img2} title={'Projeto: Previsão do tempo'} link={"https://gadarkeres.github.io/Projeto-Tempo/"}/>
         <Card img={img4}title={"Projeto: Gerador de senha e nicks"} link={"https://gadarkeres.github.io/Generate-Pass-Nick/"}/>
         <Card img={img3} title={'Projeto: Desafio validação de email'} link={"https://gadarkeres.github.io/email-validation-project/"}/>
-        <Card img={embreve}title={"EM BREVE"}/>
+        
         <Card img={embreve}title={"EM BREVE"}/>
         
       </div>
