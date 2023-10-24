420a01f5546d93521b93cb316196eea8c655fce4
Matheus Rafael <matheusrafael77@hotmail.com>
2023-10-24 15:51:06 -0300

    up

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,11 +3,11 @@ import './Projects.css'
 import Card from './Card'
 // imagens
 import img1 from './img/timer.png'
-import img2 from './img/tempo.png'
 import img4 from './img/gerador.png'
 import img5 from './img/todo-list.png'
 import img6 from './img/projeto_filme.ong.png'
 import img7 from './img/vayne.png'
+import img8 from './img/petshop-virtual.png'
 // pdf
 import pdf from './file/CV-Atualizado-Matheus.pdf'
 
@@ -19,11 +19,11 @@ const Projects = () => {
     <section id="section4">
       <h2 className="Tittle">PROJETOS</h2>
       <div className="container_card">
+      <Card img={img8} title={'Projeto: PetShop virtual'} link={"https://gadarkeres.github.io/Pet_shop/"} repositorio={"https://github.com/Gadarkeres/Pet_shop"}/>
       <Card img={img7} title={'Projeto: Site completo de personagem'} link={"https://gadarkeres.github.io/Vayne-Project/"} repositorio={"https://github.com/Gadarkeres/Vayne-Project"}/>
       <Card img={img6}title={"Projeto: Catálogo de filmes detalhado"} link={"https://gadarkeres.github.io/film_project"} repositorio={"https://github.com/Gadarkeres/film_project"}/>
       <Card img={img5}title={"Projeto: Lista de tarefas automática"} link="https://gadarkeres.github.io/todo-list/" repositorio={"https://github.com/Gadarkeres/todo-list"}/>
         <Card img={img1} title={"Projeto: Timer de estudos"} link={"https://gadarkeres.github.io/Projeto-timer/"} repositorio={"https://github.com/Gadarkeres/Projeto-timer"}/>
-        <Card img={img2} title={'Projeto: Previsão do tempo'} link={"https://gadarkeres.github.io/Projeto-Tempo/"} repositorio={"https://github.com/Gadarkeres/Projeto-Tempo"}/>
         <Card img={img4}title={"Projeto: Gerador de senha e nicks"} link={"https://gadarkeres.github.io/Generate-Pass-Nick/"} repositorio={"https://github.com/Gadarkeres/Generate-Pass-Nick"}/>
        
         
