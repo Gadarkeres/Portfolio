574cb8590c4d6b52f002a67755fd4511b12ebaf8
Matheus Rafael <matheusrafael77@hotmail.com>
2023-11-13 14:21:15 -0300

    update

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -15,7 +15,7 @@ const Contact = () => {
 
     const [loading, setLoading] = useState(false)
 
-    const[open, setOpen] = useState(false)
+    const[open, setOpen] = useState(true)
 
     const handleSubmit = (e) => {
         e.preventDefault()
